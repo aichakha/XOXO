e2cec6aeaaf382f73d463e9b92321f08e7276ab2
aichakha <kharrataicha84@gmail.com>
2025-03-05 20:35:17 +0100

    modif

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,11 +4,11 @@ import { AcceuilPage } from './acceuil/acceuil.page';
 import { SignupPage } from './auth/pages/signup/signup.page';
 import { ForgotPasswordPage } from './auth/pages/forgot-password/forgot-password.page';
 import { ResetPasswordPage } from './auth/pages/reset-password/reset-password.page';
-<<<<<<< HEAD
+
 import { HistoryPage } from './history/history.page';
-=======
+
 import { ContactPage } from './contact/contact.page';
->>>>>>> d7d5fc38fc9ae51a89110471040d064f4704f3d6
+
 
 export const routes: Routes = [
   { path: '', redirectTo: '/acceuil', pathMatch: 'full' },
@@ -16,13 +16,10 @@ export const routes: Routes = [
   { path: 'login', component: LoginPage },
   { path: 'signup', component: SignupPage },
   { path: 'forgot-password', component: ForgotPasswordPage },
-<<<<<<< HEAD
   { path: 'history', component: HistoryPage },
 
-=======
   { path: 'reset-password',component: ResetPasswordPage },
   { path: 'contact',component: ContactPage },
->>>>>>> d7d5fc38fc9ae51a89110471040d064f4704f3d6
   {
     path: 'signup',
     loadComponent: () => import('./auth/pages/signup/signup.page').then( m => m.SignupPage)
@@ -36,16 +33,16 @@ export const routes: Routes = [
     loadComponent: () => import('./auth/pages/reset-password/reset-password.page').then( m => m.ResetPasswordPage)
   },
   {
-<<<<<<< HEAD
+
     path: 'history',
     loadComponent: () => import('./history/history.page').then( m => m.HistoryPage)
   },
 
-=======
+  {
     path: 'contact',
     loadComponent: () => import('./contact/contact.page').then( m => m.ContactPage)
   },
->>>>>>> d7d5fc38fc9ae51a89110471040d064f4704f3d6
+
 
 
 ];
